5d13d4322af6a5701103445bf5e73e433e266ce1
Justin Hester <justinvhester@users.noreply.github.com>
2015-10-26 16:18:15 -0500

    refactor: Change table structure to normalize data
    Originally players table setup for id, full_name, number of wins, number
    of matches. Data about the number of matches played and number of
    matches won was stored in the matches table. Meaning the data was not
    yet normalized as it should be. A really big hint came when I realized
    one of the functions in tournaments.py was updating both tables in order
    to report matches.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -12,15 +12,18 @@ CREATE DATABASE tournament;
 
 CREATE TABLE players (
     id serial primary key,
-    full_name text,
-    wins integer DEFAULT 0,
-    matches integer DEFAULT 0
+    full_name text
+-- May be better to separate out the wins and matches data to it's own table
+-- or view per 'normalized design' in lesson 4
+--    wins integer DEFAULT 0,
+--    matches integer DEFAULT 0
     );
 
 CREATE TABLE matches (
---    p1 integer,
---    p2 integer,
-    win integer,
-    loss integer,
+-- Column one will hold the id of the winner
+    win integer references players (id),
+-- Column one will hold the id of the loser
+    loss integer references players (id),
+-- Players will not play each other twice, so the two player combination will be unique
     primary key (win, loss)
     );
