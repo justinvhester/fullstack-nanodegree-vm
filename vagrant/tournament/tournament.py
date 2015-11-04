edf1ca9c62d027f607b247e8c7798e89aa65ac0d
Justin Hester <justinvhester@users.noreply.github.com>
2015-11-03 20:48:04 -0600

    style: Cleanup whitespace per pylint
    Important to note I pylint was run on the host system (macbook) so
    certain errors [can't import psycopg2] were ignored. Also pylint gave
    errors for breaking up the list comprehension in swissPairings() but
    breaking it up makes the code more readable. I'll cite Raymond Hettinger
    as a source for placing readability over puritanical adhearance to
    pylint.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -44,10 +44,10 @@ def countPlayers():
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -99,16 +99,16 @@ def reportMatch(winner, loser):
     c.execute(QUERY, DATA)
     conn.commit()
     conn.close()
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -120,13 +120,13 @@ def swissPairings():
     pCount = countPlayers()
     # List comprehension will create a list of tuples each containing
     # info of the 'next two' players from the stnds list
-    #On the first loop through stnds[0] is the top rated player
-    pairs =  [(stnds[pInfo][0], stnds[pInfo][1],
-               #add 1 to the index of stnds to get the very next player
-               stnds[pInfo + 1][0], stnds[pInfo + 1][1])
-             # Loop through only the number of currently registered
-             # players, and only for even numbers coming out of xrange
-             for pInfo in xrange(pCount) if pInfo%2 == 0 ]
+    # On the first loop through stnds[0] is the top rated player
+    pairs = [(stnds[pInfo][0], stnds[pInfo][1],
+            # add 1 to the index of stnds to get the very next player
+            stnds[pInfo + 1][0], stnds[pInfo + 1][1])
+            # Loop through only the number of currently registered
+            # players, and only for even numbers coming out of xrange
+            for pInfo in xrange(pCount) if pInfo%2 == 0]
     return pairs
 
 
